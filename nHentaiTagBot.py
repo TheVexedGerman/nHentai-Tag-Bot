af0b1ab4ceb9f352fd9664d2c9b7338390e9f03a
TheVexedGerman <jmohr11@gmail.com>
2019-01-02 14:26:48 +0100

    Forgot += broke link recognition

--- nHentaiTagBot.py
+++ nHentaiTagBot.py
@@ -15,8 +15,8 @@ LINK_URL_EHENTAI = "https://e-hentai.org/g/"
 
 TIME_BETWEEN_PM_CHECKS = 60  # in seconds
 
-PARSED_SUBREDDIT = 'Animemes+hentai_irl+anime_irl+u_Loli-Tag-Bot+u_nHentai-Tag-Bot+HentaiSource'
-# PARSED_SUBREDDIT = 'loli_tag_bot'
+# PARSED_SUBREDDIT = 'Animemes+hentai_irl+anime_irl+u_Loli-Tag-Bot+u_nHentai-Tag-Bot+HentaiSource'
+PARSED_SUBREDDIT = 'loli_tag_bot'
 
 nhentai = 0
 tsumino = 1
@@ -230,8 +230,8 @@ def analyseNumberEhentai(galleryNumberAndToken):
 def authenticate():
     print("Authenticating...")
     reddit = praw.Reddit(
-        'nhentaibot'
-        # 'thevexedgermanbot'
+        # 'nhentaibot'
+        'thevexedgermanbot'
     )
     print("Authenticated as {}".format(reddit.user.me()))
     return reddit
@@ -512,7 +512,7 @@ def scanForURL(comment):
     tsuminoNumbers = removeDuplicates(tsuminoNumbers)
 
     ehentaiLinks = re.findall(r'https?:\/\/(?:www.)?e-hentai.org\/g\/\d{1,8}\/\w*', comment)
-    ehentaiLinks = re.findall(r'https?:\/\/(?:www.)?exhentai.org\/g\/\d{1,8}\/\w*', comment)
+    ehentaiLinks += re.findall(r'https?:\/\/(?:www.)?exhentai.org\/g\/\d{1,8}\/\w*', comment)
     try:
         for link in ehentaiLinks:
             removeURL = re.search(r'(?<=\/g\/).+', link).group(0)
@@ -566,6 +566,7 @@ def getEhentaiNumbers(comment):
             numbers.append([galleryID, galleryToken])
     except AttributeError:
         print("Number Recognition failed Ehentai")
+    print(numbers)
     return numbers
 
 
@@ -736,11 +737,11 @@ def processCommentReply(comment, reddit):
             #     numbers = []
 
 
-if __name__ == '__main__':
-    while True:
-        try:
-            main()
-        except Exception as e:
-            pass
+# if __name__ == '__main__':
+#     while True:
+#         try:
+#             main()
+#         except Exception as e:
+#             pass
 
-# main()
+main()
