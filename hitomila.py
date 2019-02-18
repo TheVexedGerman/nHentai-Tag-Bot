3f6591cc576b6e82e0f3a706c3c4538c4b1fd887
TheVexedGerman <jmohr11@gmail.com>
2019-02-18 18:07:59 +0100

    Added generateReplyString and getNumbers for hitomi.la

--- hitomila.py
+++ hitomila.py
@@ -19,8 +19,15 @@ def analyseNumber(galleryNumber):
     isRedacted = False
 
     response = requests.get(API_URL_HITOMILA+str(galleryNumber)+".html")
+    if response.status_code == 404:
+        return [404]
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, features="html.parser")
+
+        # Check for redirect
+        if soup.title.string == 'Redirect':
+            return ['Redirect']
+
         # reduce the html to the most relevant part
         relevant = soup.find('div', class_="gallery")
         try:
@@ -77,6 +84,61 @@ def analyseNumber(galleryNumber):
         except:
             print("No Tags")
 
-        return [title, numberOfPages, artist, group, types, language, series, characters, tags, isRedacted]
+        return [title, numberOfPages, [artist], group, types, language, series, characters, tags, isRedacted]
     else:
         return []
+
+def generateReplyString(processedData, galleryNumber):
+    title = 0
+    pages = 1
+    artist = 2
+    group = 3
+    types = 4
+    language = 5
+    series = 6
+    characters = 7
+    tags = 8
+    isRedacted = 9
+    replyString = ''
+
+    if processedData:
+        if not len(processedData) > 2:
+            replyString += ">Hitomi.la: " + str(galleryNumber) + "\n\n"
+            if processedData[0] == 'Redirect':
+                replyString += "This gallery is trying to redirect and therfore doesn't exist anymore. Please try a different one."
+                return replyString
+            if processedData[0] == 404:
+                replyString += "This gallery is returning a 404. The gallery has either been removed or doesn't exist yet."
+        if processedData[isRedacted]:
+            replyString += ">Hitomi.la: [REDACTED]\n\n"
+        else:
+            replyString += ">Hitomi.la: " + str(galleryNumber) + "\n\n"
+        if processedData[title]:
+            replyString += "**Title**: " + processedData[title] + "\n\n"
+        if processedData[pages] > 0:
+            replyString += "**Number of pages**: " + str(processedData[pages]) + "\n\n"
+        
+        if processedData[artist]:
+            replyString += commentpy.additionalTagsString(processedData[artist], "Artist", False) + "\n\n"
+        if processedData[group]:
+            replyString += commentpy.additionalTagsString(processedData[group], "Group", False) + "\n\n"
+        if processedData[types]:
+            replyString += commentpy.additionalTagsString(processedData[types], "Type", False) + "\n\n"
+        if processedData[language]:
+            replyString += commentpy.additionalTagsString(processedData[language], "Language", False) + "\n\n"
+        if processedData[series]:
+            replyString += commentpy.additionalTagsString(processedData[series], "Series", False) + "\n\n"
+        if processedData[characters]:
+            replyString += commentpy.additionalTagsString(processedData[characters], "Characters", False) + "\n\n"
+        if processedData[tags]:
+            replyString += commentpy.additionalTagsString(processedData[tags], "Tag", False) + "\n\n"
+    return replyString
+
+def getNumbers(comment):
+    numbers = re.findall(r'(?<=\!)\d{5,8}(?=\!', comment)
+    try:
+        numbers = [int(number) for number in numbers]
+    except ValueError:
+        numbers = []
+    numbers = commentpy.removeDuplicates(numbers)
+    return numbers
