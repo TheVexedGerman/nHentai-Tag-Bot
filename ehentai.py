2ae9904745daa5657ad9e379ac89c17b4cec51ad
TheVexedGerman <jmohr11@gmail.com>
2019-03-24 19:37:23 +0100

    Make relative imports work.

--- ehentai.py
+++ ehentai.py
@@ -1,3 +1,5 @@
+# from https://stackoverflow.com/questions/16981921/relative-imports-in-python-3 to make the imports work when imported as submodule
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import commentpy
 import requests
 import json
